5cbeba122d7c5b2767c7bc9a8f89f5c84805ee69
Rachit1313 <58596563+Rachit1313@users.noreply.github.com>
2020-05-20 14:22:48 +0530

    Update padmin.py

--- userbot/plugins/padmin.py
+++ userbot/plugins/padmin.py
@@ -49,7 +49,7 @@ async def _(event):
             "**(8) Change Chat Info: ☑️**",
             "**(8) Change Chat Info: ✅**",
             "**Permission Granted Successfully**",
-            "**pRoMooTeD SuCcEsSfUlLy bY**{DEFAULTUSER}"
+            "**pRoMooTeD SuCcEsSfUlLy bY Your Father**"
 
  ]
 
