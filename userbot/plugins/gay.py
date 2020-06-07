25106141d0708694e861b4dc4ad409a1243d435b
Rachit1313 <58596563+Rachit1313@users.noreply.github.com>
2020-06-08 03:24:36 +0530

    Create gay.py

---
+++ userbot/plugins/gay.py
@@ -0,0 +1,9 @@
+from telethon import events
+from datetime import datetime
+
+
+@command(pattern="^.gay")
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("┈┈┈╭━━━━━╮┈┈┈┈┈\n┈┈┈┃┊┊┊┊┊┃┈┈┈┈┈\n┈┈┈┃┊┊╭━╮┻╮┈┈┈┈\n┈┈┈╱╲┊┃▋┃▋┃┈┈┈┈\n┈┈╭┻┊┊╰━┻━╮┈┈┈┈\n┈┈╰┳┊╭━━━┳╯┈┈┈┈\n┈┈┈┃┊┃╰━━┫┈U GAY\n┈┈┈┈┈┈┏━┓┈┈┈┈┈┈")
